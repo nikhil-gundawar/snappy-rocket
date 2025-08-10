ec4135af27ee48b2b34b72a8df9a0670e6ad093a
Nikhil Gundawar <nikhil.gundawar92@gmail.com>
2025-08-10 16:57:46 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+
+const CACHE_NAME = 'flappy-rocket-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
